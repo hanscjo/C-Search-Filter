82fff627a14d59a5c630313df88df40fe0a07da7
hanscjo <44261306+hanscjo@users.noreply.github.com>
2023-05-12 13:05:24 +0200

    Add files via upload

---
+++ Uke2/Uke2/Uke2/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Uke2
+{
+    internal class Program
+    {
+        private static Student[] students;
+        static void Main(string[] args)
+        {
+            InitializeStudents();
+            ViewCourseDistribution();
+        }
+
+        private static void InitializeStudents()
+        {
+            Student student1 = new Student("Kjellbjørg", 46, new string[]{"Padling", "Symaskinreparasjon", "Kryssord"});
+            Student student2 = new Student("Gullfrid", 58, new string[] { "Kattepass", "Symaskinreparasjon", "Kryssord"});
+            Student student3 = new Student("Julius", 23, new string[] { "Erobring", "Kattepass", "Show-business"});
+            Student student4 = new Student("Alexander", 35, new string[] { "Erobring", "Symaskinreparasjon", "Padling" });
+
+            students = new Student[] {student1, student2, student3, student4};
+        }
+
+        private static void ViewCourseDistribution()
+        {
+            while (true)
+            {
+                Console.WriteLine("Skriv inn et kurs for å se hvilke studenter som tar det...");
+                CheckGivenCourse();
+                
+                Console.WriteLine(" ");
+            }
+            
+        }
+
+        private static void CheckGivenCourse()
+        {
+            string chosenCourse = Console.ReadLine();
+            int courseTakerCount = 0;
+            Console.WriteLine("Sjekker etter følgende kurs: " + chosenCourse + "...");
+
+            foreach (Student student in students)
+            {
+                if (Array.Exists(student.Kurs, course => course == chosenCourse))
+                {
+                    if (courseTakerCount == 0)
+                    {
+                        Console.WriteLine("Følgende studenter tar " + chosenCourse + ":");
+                    }
+                    courseTakerCount++;
+                    PrintGivenStudent(student);
+                }
+
+            }
+
+            if (courseTakerCount == 0)
+            {
+                Console.WriteLine("Ingen tar " + chosenCourse + ".");
+            }
+        }
+
+        private static void PrintGivenStudent(Student givenStudent)
+        {
+            Console.WriteLine("Navn: " + givenStudent.Navn + ", alder: " + givenStudent.Alder);
+        }
+    }
+}
---
+++ Uke2/Uke2/Uke2/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uke2
+{
+    internal class Student
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+        public string[] Kurs { get; set; }
+
+
+        public Student(string navn, int alder, string[] kurs) {
+            Navn = navn;
+            Alder = alder;
+            Kurs = kurs;
+        }
+    }
+}
